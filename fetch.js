7b357b1c3f1b4bae32719c9e7b25dc103a778359
vijay sagar gooty <31399807+vijaysagar111@users.noreply.github.com>
2020-08-21 21:14:48 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,81 @@
+function page(event){
+    var page = event.target.innerHTML.toLowerCase();
+    var sectionDiv = document.getElementById('section');
+    sectionDiv.innerText = "";
+    
+    getData(page);
+}
+
+
+async function getData(page){
+
+    var data = await (await fetch(`https://api.nytimes.com/svc/topstories/v2/${page}.json?api-key=xZrFIeI5FcAZ2uZ3HIZk5T5cTwHPqOl6`)).json();
+    
+    let results =  data.results;
+    console.log(results);
+
+	for(let i = 0 ; i < 10 ; i++){
+
+		var story = results[i];
+		
+        var sectionDiv = document.getElementById('section');
+        
+
+		var cardDiv = document.createElement("div");
+		cardDiv.setAttribute("class","card  mb-3 ");
+
+		var cardBodyDiv = document.createElement("div");
+		cardBodyDiv.setAttribute("class","card-body");
+
+		var cardBodyRowDiv = document.createElement("div");
+		cardBodyRowDiv.setAttribute("class","row");
+
+		var cardBodyRowCol8Div = document.createElement("div");
+		cardBodyRowCol8Div.setAttribute("class","col-sm-8");
+
+        var newsCategory = document.createElement("h5");
+        newsCategory.innerHTML = story.section;
+        newsCategory.classList = "card-title sectioncard";
+
+		var storyTitle = document.createElement("h3");
+		storyTitle.innerHTML = story.title;
+        storyTitle.classList = "titlecard";
+
+        var storyDate = document.createElement("h3");
+        let dateObject = new Date(Date.parse(story.created_date));
+        storyDate.innerText =  dateObject.toLocaleString('default', { month: 'short' }) + " " + dateObject.getDate();
+        
+        
+        storyDate.className = "datecard";
+
+		var storyAbstract = document.createElement("h3");
+        storyAbstract.innerHTML = story.abstract;
+        storyAbstract.className = "abstractcard";
+
+        var continueReading = document.createElement("a");
+        continueReading.className = "text-muted";
+        continueReading.innerText = "continue Reading";
+        continueReading.className = "continueReading";
+        continueReading.href = story.short_url;
+        cardBodyRowCol8Div.appendChild(newsCategory);
+		cardBodyRowCol8Div.appendChild(storyTitle);
+		cardBodyRowCol8Div.appendChild(storyDate);
+        cardBodyRowCol8Div.appendChild(storyAbstract);
+        cardBodyRowCol8Div.appendChild(continueReading);
+        
+
+		var cardBodyImgCol4Div = document.createElement("img");
+		cardBodyImgCol4Div.setAttribute("class","col-sm-4 img-thumbnail");
+		cardBodyImgCol4Div.setAttribute("src",story.multimedia[0].url);
+
+		cardBodyRowDiv.appendChild(cardBodyRowCol8Div);
+		cardBodyRowDiv.appendChild(cardBodyImgCol4Div);
+
+		cardBodyDiv.appendChild(cardBodyRowDiv);
+
+		cardDiv.appendChild(cardBodyDiv);
+
+		sectionDiv.appendChild(cardDiv);
+	}
+}
+
